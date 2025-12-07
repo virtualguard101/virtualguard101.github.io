eb8d1081e758df1a62118d7b22c24056ade67beb
VIRTUALGUARD <virtualguard101@gmail.com>
2025-12-07 22:05:46 +0800

    build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1765116252942";const preCache = ["/images/taichi.png","https://image.tmdb.org/t/p/original/c0y8ScAJnSs5xMZ9b1lm2k9gSwz.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1765116346152";const preCache = ["/images/taichi.png","https://image.tmdb.org/t/p/original/c0y8ScAJnSs5xMZ9b1lm2k9gSwz.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
